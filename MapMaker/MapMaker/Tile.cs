b0c58a6af98a410a791a29dff7c25ade7e838e44
Alcanteria <nick@alcanteria.com>
2015-10-31 17:07:19 -0400

    Tried to make sure the tile class commits.

---
+++ MapMaker/MapMaker/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapMaker
+{
+    /* This class represents a single square on the map grid. It has its own image defined separately from
+     other tiles on the map/grid. */
+    class Tile
+    {
+        // This is to just make sure this file is actually included in the commit.
+        private int PLACEHOLDER = 29837;
+    }
+}
